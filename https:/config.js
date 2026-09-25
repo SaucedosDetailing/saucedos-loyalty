d1929c009fc17e5862695e0760d1f6d5d9d566dc
SaucedosDetailing <saucedosdetailing26@gmail.com>
2026-09-24 23:36:36 -0600

    Add Supabase configuration to config.js

---
+++ https:/config.js
@@ -0,0 +1,2 @@
+window.SUPABASE_URL = "AQUI_VA_TU_API_URL"https://pstrbocalwkzrtgvnnwy.supabase.co/rest/v1/;
+window.SUPABASE_ANON_KEY = "AQUI_VA_TU_LLAVE_PUBLICA"sb_publishable_ieBuaxoCi6L6Oe6lrzC8xw_yX_wZKVZ;
